f420d58d41ad9e270a2295480ac472093733f337
jackbackrack <jrb@pobox.com>
2016-09-27 16:03:59 -0700

    add libs

--- Makefile.mk
+++ Makefile.mk
@@ -1,3 +1,12 @@
+ifeq ($(OS2), Darwin)
+  LIBS += -lglfw3 -framework Carbon -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
+else
+  OS := $(strip $(shell uname -o))
+  ifeq ($(OS), GNU/Linux)
+    LIBS += -lglfw -lrt -lm -lXrandr -lXinerama -lXi -lXcursor -lXrender -lGL -lGLU -lpthread -ldl -ldrm -lXdamage -lXfixes -lX11-xcb -lxcb-glx -lxcb-dri2 -lxcb-dri3 -lxcb-present -lxcb-randr -lxcb-xfixes -lxcb-render -lxcb-shape -lxcb-sync -lxshmfence -lXxf86vm -lXext -lX11 -lxcb -lXau -lXdmcp
+  endif
+endif
+
 ${GEN}/gl.pkg: stanza-gfx/gl.stanza ${GEN}/utils.pkg 
 	stanza $< $(STZ_FLAGS) -pkg ${GEN}
 

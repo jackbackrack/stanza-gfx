177d5b7ea14bc444a8a0bf289f3c30d3c8e3d355
jackbackrack <jrb@pobox.com>
2016-12-31 20:58:05 -0800

    new stanza

--- Makefile.mk
+++ Makefile.mk
@@ -12,17 +12,17 @@ stanza_gfx_all: ${GEN}/gl.pkg ${GEN}/glu.pkg ${GEN}/glfw.pkg ${GEN}/font.pkg ${G
 ALL_PKG_DEPS += stanza_gfx_all
 
 ${GEN}/gl.pkg: stanza-gfx/gl.stanza ${GEN}/utils.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 
 ${GEN}/glu.pkg: stanza-gfx/glu.stanza ${GEN}/gl.pkg ${GEN}/geom.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 
 ${GEN}/glfw.pkg: stanza-gfx/glfw.stanza ${GEN}/utils.pkg ${GEN}/gl.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 
 ${GEN}/font.pkg: stanza-gfx/font.stanza ${GEN}/utils.pkg ${GEN}/geom.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 
 ${GEN}/gfx.pkg: stanza-gfx/gfx.stanza ${GEN}/gl.pkg ${GEN}/glfw.pkg ${GEN}/font.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 

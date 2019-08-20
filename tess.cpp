45d51aeb7d3c8651cd86c173ffc953026658f1b4
jackbackrack <jrb@pobox.com>
2019-08-20 16:02:43 -0700

    update to use cpp version of tess

---
+++ tess.cpp
@@ -0,0 +1,113 @@
+#ifdef MACOSX
+// #include "/System/Library/Frameworks/OpenGL.framework/Headers/glu.h"
+#include <OpenGL/glu.h>
+#else
+#include <GL/gl.h>
+#endif
+#include <vector>
+
+typedef struct tess_vec2      { float x, y; } tess_vec2;
+typedef struct tess_vec3      { float x, y, z; } tess_vec3;
+typedef struct tess_vec3d     { double x, y, z; } tess_vec3d;
+typedef struct tess_tri       { uint32_t a, b, c; } tess_tri;
+
+typedef struct tess_mesh {
+    tess_vec3* verts;
+    tess_tri* tris;
+    uint32_t tri_count;
+    uint32_t vert_count;
+} tess_mesh;
+
+typedef struct tess_contour {
+    tess_vec2* pts;
+    uint32_t count;
+} tess_contour;
+
+typedef struct tess_contours {
+    tess_contour* cs;
+    uint32_t count;
+} tess_contours;
+
+void tess_mesh_delete(tess_mesh* m) {
+  delete [] m->verts;
+  delete [] m->tris;
+  delete m;
+}
+
+typedef struct tess_mesh_data {
+  std::vector<int> indices;
+  std::vector<tess_vec3> vertices;
+} tess_mesh_data;
+
+static tess_mesh_data mesh;
+
+int vertex_callback (int index, int mesh_index) {
+  mesh.indices.push_back(index);
+  return 0;
+}
+
+static int begin_callback (int index, int mesh_index) {
+  return 0;
+}
+
+static int edge_flag_callback (int index, int mesh_index) {
+  return 0;
+}
+
+static int end_callback (int mesh_index) {
+  return 0;
+}
+
+static int error_callback (int errno, int mesh_index) {
+  return 0;
+}
+
+static int combine_callback (double* coords, int* vertex_data, float* weight, int* out_data, int mesh_index) {
+  tess_vec3 vec = { (float)coords[0], (float)coords[1], (float)coords[2] };
+  mesh.vertices.push_back(vec);
+  return mesh.vertices.size() - 1;
+}
+
+tess_mesh* glu_triangulate (float* poly) {
+  auto tess = gluNewTess();
+  gluTessCallback(tess, GLU_TESS_BEGIN,   (void (*)())(&begin_callback));
+  gluTessCallback(tess, GLU_TESS_END,     (void (*)())(&end_callback));
+  gluTessCallback(tess, GLU_TESS_VERTEX,  (void (*)())(&vertex_callback));
+  gluTessCallback(tess, GLU_TESS_COMBINE, (void (*)())(&combine_callback));
+  gluTessCallback(tess, GLU_TESS_ERROR,   (void (*)())(&error_callback));
+  mesh.vertices.clear();
+  mesh.indices.clear();
+  gluTessBeginPolygon(tess, 0);
+  int poly_count = (int)(*poly++);
+  for (int i = 0; i < poly_count; i++) {
+    int contour_count = (int)(*poly++);
+    gluTessBeginContour(tess);
+    for (int j = 0; j < contour_count; j++) {
+      tess_vec3 pt = { (*poly++), (*poly++), (*poly++) };
+      mesh.vertices.push_back(pt);
+      tess_vec3d pt3 = { (double)pt.x, (double)pt.y, (double)pt.z };
+      gluTessVertex(tess, (double*)(&pt3.x), (void*)(mesh.vertices.size() - 1));
+    }
+    gluTessEndContour(tess);
+  }
+  gluTessEndPolygon(tess);
+  gluDeleteTess(tess);
+  auto out = new tess_mesh;
+  out->verts = new tess_vec3[mesh.vertices.size()];
+  out->vert_count = mesh.vertices.size();
+  out->tris = new tess_tri[mesh.indices.size() / 3];
+  out->tri_count = mesh.indices.size() / 3;
+  int i;
+  i=0;
+  for (auto& v : mesh.vertices) {
+    out->verts[i++] = {v.x, v.y, v.z};
+  }
+  i=0;
+  for (int j = 0; j < mesh.indices.size(); j = j + 3) {
+    out->tris[i++] = {(uint32_t)mesh.indices[j + 0], (uint32_t)mesh.indices[j + 1], (uint32_t)mesh.indices[j + 2]};
+  }
+
+  return out;
+  
+}
+
